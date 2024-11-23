ac40ed2ed789e8a42523d5ecc537f551ae723590
Yarelis Martin <yarelis0martin@gmail.com>
2024-11-23 12:45:48 -0600

    created an anonymous dto that will be returned for get single user

--- Bookworm-Society-API/Interfaces/IUserService.cs
+++ Bookworm-Society-API/Interfaces/IUserService.cs
@@ -6,7 +6,7 @@ namespace Bookworm_Society_API.Interfaces
     public interface IUserService
 
     {
-        Task<Result<User>> GetUserByIdAsync(int userId);
+        Task<Result<object>> GetUserByIdAsync(int userId);
         Task<Result<User>> CreateUserAsync(User user);
         Task<Result<User>> UpdateUserAsync(User user, int userId);
     }
