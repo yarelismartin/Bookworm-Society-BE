f428d57f1633776d69463ef441af10448afddaf9
Yarelis Martin <yarelis0martin@gmail.com>
2024-12-17 15:13:01 -0600

    final changes were made to paths

--- Bookworm-Society-API/Interfaces/IVotingSessionRepository.cs
+++ Bookworm-Society-API/Interfaces/IVotingSessionRepository.cs
@@ -8,7 +8,6 @@ namespace Bookworm_Society_API.Interfaces
         Task<VotingSession> GetLatestVotingSessionAsync(int bookClubId, int userId);
         Task<bool> IsUserAllowedToVote(int bookClubId, int userId);
         Task<VotingSession> CreateVotingSession(VotingSession votingSession);
-
         Task<List<Book>> GetBooksByIdsAsync(List<int> bookIds);
         Task<List<VotingSession>> GetActiveVotingSessions(CancellationToken cancellationToken);
         Task<int> CalculateWinningBook(List<Vote> votes);
