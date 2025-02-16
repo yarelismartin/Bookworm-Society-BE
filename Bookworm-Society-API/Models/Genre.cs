d047dea3c4394a30ae797a5d3b3a89c8aa97a695
Yarelis Martin <yarelis0martin@gmail.com>
2025-02-15 18:56:28 -0600

    added the genre model

---
+++ Bookworm-Society-API/Models/Genre.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookworm_Society_API.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Book>? Books { get; set; }
+    }
+}
