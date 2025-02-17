206fdcbe1934bac245a4e2ea6d8acbb50a9958df
Yarelis Martin <yarelis0martin@gmail.com>
2025-02-17 15:14:29 -0600

    updataed methods in my service and repository layer so that I can include the author's name and the genre when returning a book obj to my client

--- Bookworm-Society-API/Migrations/20250216020633_BookModelUpdate.Designer.cs
+++ Bookworm-Society-API/Migrations/20250216020633_BookModelUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Bookworm_Society_API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Bookworm_Society_API.Migrations
 {
     [DbContext(typeof(Bookworm_SocietyDbContext))]
-    partial class Bookworm_SocietyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250216020633_BookModelUpdate")]
+    partial class BookModelUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
