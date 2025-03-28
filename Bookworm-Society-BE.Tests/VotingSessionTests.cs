e8bbb142b8416a68c0ea923f02b5b8d42416d2f0
Yarelis Martin <yarelis0martin@gmail.com>
2025-03-27 22:39:06 -0500

    test was created that grabs the latest voting session for a book club

--- Bookworm-Society-BE.Tests/VotingSessionTests.cs
+++ Bookworm-Society-BE.Tests/VotingSessionTests.cs
@@ -29,6 +29,10 @@ namespace Bookworm_Society_BE.Tests
         public async Task GetBookClubLatestVotingSession_ReturnsVotingSession_WhenBookClubExist()
         {
             //ARANGE
+            //two varibles to pass to service methood, club id and user id 
+            int bookClubId = 3;
+            int userId = 2;
+
             //object being returned from service method 
             var serviceReturn = new
             {
@@ -36,9 +40,22 @@ namespace Bookworm_Society_BE.Tests
                 IsActive = true,
                 VotingStartDate = DateTime.Now,
                 VotingEndDate = DateTime.Now,
-                HasUserVotes = true,
-                VotingBooks = new List<object>()
+                HasUserVoted = true,
+                VotingBooks = new List<object>
+                    {
+                        new
+                        {
+                            Id = 1,
+                            Title = "Book Title",
+                            Description = "Some Description",
+                            Author = "John Doe",
+                            Genre = "Fiction",
+                            ImageUrl = "http://example.com/image.jpg",
+                            TotalVotes = 5
+                        }
+                    }
             };
+
             //repo returns a voting session 
             var repoReturn = new VotingSession
             {
@@ -46,26 +63,45 @@ namespace Bookworm_Society_BE.Tests
                 IsActive = true,
                 VotingStartDate = DateTime.Now,
                 VotingEndDate = DateTime.Now,
-                VotingBooks = new List<Book>()
+                BookClubId = bookClubId,
+                VotingBooks = new List<Book>
+                {
+                    new Book
+                    {
+                        Id = 1,
+                        Title = "Book Title",
+                        Description = "Some Description",
+                        Author = new Author { FirstName = "John", LastName = "Doe" },
+                        Genre = new Genre { Name = "Fiction" },
+                        ImageUrl = "http://example.com/image.jpg"
+                    }
+                },
+                Votes = new List<Vote>
+                    {
+                        new Vote { UserId = userId, BookId = 1 },
+                        new Vote { UserId = 3, BookId = 1 },
+                        new Vote { UserId = 4, BookId = 1 },
+                        new Vote { UserId = 5, BookId = 1 },
+                        new Vote { UserId = 6, BookId = 1 }
+                    }
 
-                };
-            //two varibles to pass to service methood, club id and user id 
-            int bookClubId = 3;
-            int userId = 2;
-            //get latest voting session
+            };
+
+
             _mockVotingSession.Setup(repo => repo.GetLatestVotingSessionAsync(bookClubId, userId)).ReturnsAsync(repoReturn);
-                //user exist 
-                _mockBaseRepository.Setup(repo=> repo.UserExistsAsync(userId)).ReturnsAsync(true);
-                //book exist 
-                _mockBaseRepository.Setup(repo => repo.BookExistsAsync(bookClubId)).ReturnsAsync(true);
-                // is user allowed to vote 
-                _mockVotingSession.Setup(repo => repo.IsUserAllowedToVote(bookClubId, userId)).ReturnsAsync(true);
+            _mockBaseRepository.Setup(repo=> repo.UserExistsAsync(userId)).ReturnsAsync(true);
+            _mockBaseRepository.Setup(repo => repo.BookClubExistsAsync(bookClubId)).ReturnsAsync(true);
+            _mockVotingSession.Setup(repo => repo.IsUserAllowedToVote(bookClubId, userId)).ReturnsAsync(true);
+
             //ACT
             var result = await _votingSessionService.GetLatestVotingSessionAsync(bookClubId, userId);
+
             //ASSERT
             result.Should().NotBeNull();
-            result.Data.Should().BeEquivalentTo(serviceReturn);
-                
+            result.Data.Should().BeEquivalentTo(serviceReturn, options =>
+                    options.Excluding(ctx => ctx.VotingStartDate)
+                           .Excluding(ctx => ctx.VotingEndDate) // Exclude DateTime to prevent precision issues
+                );
         }
 
         [Fact]
