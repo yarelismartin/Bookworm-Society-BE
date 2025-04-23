5400c85351d8fdc3d167e391f072f2b196d4e784
Yarelis Martin <yarelis0martin@gmail.com>
2025-04-22 19:50:38 -0500

    created the hub class that will handle the client and server communication

---
+++ Bookworm-Society-API/SignalR/NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Bookworm_Society_API.SignalR
+{
+    public sealed class NotificationHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("ReceivedMessage", $"{Context.ConnectionId} has joined");
+        }
+    }
+}
